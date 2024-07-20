e7c343cbf8de97768441013fcbaef0ed36885102
Rodrigo Roman <rodrigoroman.ri@gmail.com>
2024-07-19 22:36:13 -0300

    trabalhos

---
+++ world.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var min_x = 22
+var max_x = 1120
+var min_y = 72
+var max_y = 628
+
+func _physics_process(delta):
+	var player = $CharacterBody2D
+
+
+# FECHAR
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+
+
+# CLAMP
+	player.position.x = clamp(player.position.x, min_x, max_x)
+	player.position.y = clamp(player.position.y, min_y, max_y)
