d93804aedc8594c3b612f92b9d30495d915aecd1
Tico <thiagompedroso@gmail.com>
2024-07-21 01:33:34 -0300

    laser_funcionando
    opa

--- globalvar.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-signal emitirposição(global_position: Vector2)
--- laser.gd
+++ laser.gd
@@ -6,19 +6,19 @@ const COMPRIMENTO_MÁXIMO = 2000
 @onready var fim = $Fim
 @onready var ray_cast_2d = $RayCast2D
 @onready var player = $"../player"
-
-
+var final_laser = null
 
 
 func _physics_process(delta: float) -> void:
-	var posição_mouse = (player.global_position - global_position)
-	var max_cast_para = posição_mouse * COMPRIMENTO_MÁXIMO
-	ray_cast_2d.target_position = max_cast_para
+	if Input.is_action_pressed("ação"):
+		final_laser = player.global_position - global_position
+		_laser()
+	
+func _laser(): 
+	ray_cast_2d.target_position = final_laser
 	if ray_cast_2d.is_colliding():
 		fim.global_position = ray_cast_2d.get_collision_point()
 	else:
-		fim.global_position = ray_cast_2d.target_position
+		fim.global_position = player.global_position
 	feixo.rotation = ray_cast_2d.target_position.angle()
 	feixo.region_rect.end.x = fim.position.length()
-	
-
